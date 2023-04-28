8e5fd7edbf759f7e69e9e2734550b60baf18aeac
Sydnei Davis <sydnei.dev@gmail.com>
2023-04-27 20:41:55 -0400

    built foundation of calculator

--- public/main.js
+++ public/main.js
@@ -2,10 +2,21 @@ const billAmountInput = document.getElementById("billAmount")
 const tipRangeInput = document.getElementById("tipRange")
 const calculateButton = document.getElementById("calculateButton").addEventListener("click", calculateBill)
 const totalBill = document.querySelector(".totalBill")
+const count = document.querySelector(".count")
 
-//calculateBill function 
+tipRangeInput.defaultValue = 0 
+count.innerText = tipRangeInput.value 
+tipRangeInput.addEventListener("input", (e) => count.innerText = e.target.value)
+
+//Calculate the bill
 function calculateBill(){
-const billAmount = parseFloat(billAmountInput.value)
-const tipPercentage = parseFloat(tipRangeInput.value)
+const billAmount = Number(billAmountInput.value)
+console.log(tipRangeInput.value)
+const tipPercentage = tipRangeInput.value / 100
+const tipAmount = billAmount * tipPercentage
+const totalAmount = billAmount + tipAmount 
+totalBill.innerText = totalAmount
 }
+
+//Tip Range 
 
