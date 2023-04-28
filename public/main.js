76acd8f89f8c720fbff77d33bc1a2c70a68a0adc
Sydnei Davis <sydnei.dev@gmail.com>
2023-04-27 21:11:02 -0400

    initial commit

--- public/main.js
+++ public/main.js
@@ -1,24 +1,21 @@
+const billAmountInput = document.getElementById("billAmount")
+const tipRangeInput = document.getElementById("tipRange")
+const calculateButton = document.getElementById("calculateButton").addEventListener("click", calculateBill)
+const totalBill = document.querySelector(".totalBill")
+const count = document.querySelector(".count")
 
+tipRangeInput.defaultValue = 0 
+count.innerText = tipRangeInput.value 
+tipRangeInput.addEventListener("input", (e) => count.innerText = e.target.value)
 
-const tip =  document.querySelector('#tip')
-const displayText = document.querySelector('.current-number')
-displayText.innerText = tip.value
-document.querySelector('.btn').addEventListener('click', calculatebtn)
-tip.addEventListener('input', (e) => displayText.innerText = e.target.value)
-
-
-function calculatebtn(){
-const total = document.querySelector('.total')
-const billAmount = Number(document.querySelector('.form-control').value)
-if(!billAmount){
-  total.innerText = 'Enter bill amount'
-  return 
+//Calculate the bill
+function calculateBill(){
+const billAmount = Number(billAmountInput.value)
+const tipPercentage = tipRangeInput.value / 100
+const tipAmount = billAmount * tipPercentage
+const totalAmount = billAmount + tipAmount 
+totalBill.innerText = totalAmount
 }
 
-const newTip = Number(tip.value)
+//Tip Range 
 
-console.log({billAmount, newTip})
-  // billAmount * tip 
-  total.innerText = `Total  ${billAmount + (billAmount * (newTip/100))}`
-
-}
